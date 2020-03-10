5ce02ac8a7f18649b5e98342aa8c7d19efa8f9ed
Tristan Grovender <therealtristangrovender@gmail.com>
2020-03-10 16:16:19 -0500

    MVP complete

--- js/index.js
+++ js/index.js
@@ -29,13 +29,6 @@ window.addEventListener("load", event => {
     console.log("page is fully loaded");
 });
 
-// focus event on home button
-const home = document.querySelector(".nav-link");
-// console.log(home);
-home.addEventListener("focus", function(event) {
-    event.target.style.color = "purple";
-});
-
 // window resize event
 window.addEventListener("resize", function() {
     alert("You have resized the window");
@@ -51,21 +44,47 @@ const navLinks = document.querySelectorAll(".nav-link");
 // About us color
 navLinks[1].addEventListener("click", function(event) {
     event.target.style.color = "crimson";
+    event.preventDefault();
 });
 
 // Blog color
 navLinks[2].addEventListener("dblclick", function(event) {
     event.target.style.color = "blue";
+    event.preventDefault();
 });
 
 // Contact color
 navLinks[3].addEventListener("click", function(event) {
     event.target.style.color = "orange";
+    event.preventDefault();
 });
 
 // Welcome title
 const welcomeTitle = document.querySelector(".intro h2");
-console.log(welcomeTitle);
+// console.log(welcomeTitle);
 welcomeTitle.addEventListener("mousemove", function(event) {
     event.currentTarget.style.color = "purple";
+});
+
+// focus event on home button
+const home = document.querySelector(".nav-link");
+// console.log(home);
+home.addEventListener("focus", function(event) {
+    event.target.style.color = "purple";
+    event.preventDefault();
+});
+
+// Propogation
+const body = document.querySelector("body");
+body.addEventListener("click", function(event) {
+    event.target.style.color = "purple";
+    console.log("Body is firing also!!!");
+});
+
+// turns buttons green - showcases stopPropogation
+const button = document.querySelector(".btn");
+button.addEventListener("click", function(event) {
+    event.target.style.color = "green";
+    console.log("Button is firing!");
+    event.stopPropagation();
 });
